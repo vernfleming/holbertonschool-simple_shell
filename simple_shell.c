80f04e5b377a96d2499b50173b5f695976e69f54
“vernfleming” <10568@holbertonstudents.com>
2025-04-14 22:11:52 +1000

    simple shell and updates

---
+++ simple_shell.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+/**
+ * main - Simple UNIX shell
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
+	int status;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			free(line);
+			break;
+		}
+
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
+
+		if (line[0] == '\0')
+			continue;
+
+		pid = fork();
+		if (pid == 0)
+		{
+			char *argv[] = {line, NULL};
+
+			if (execve(line, argv, environ) == -1)
+			{
+				perror("./shell");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid > 0)
+		{
+			wait(&status);
+		}
+		else
+		{
+			perror("fork");
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return (0);
+}
