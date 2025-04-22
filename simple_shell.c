2fd77c5a6c1c2cc0376f66aedab346d64ed0b7bb
“vernfleming” <10568@holbertonstudents.com>
2025-04-22 13:15:35 +1000

    0.3 PATH handling

--- simple_shell.c
+++ simple_shell.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include "version.h"
 
 extern char **environ;
@@ -47,14 +50,57 @@ char **split_line(char *line)
 }
 
 /**
- * main - Simple UNIX shell 0.2 with arguments
+ * find_command - checks if command exists
+ * @cmd: shell command name
+ * Return: full path to command
+ */
+
+char *find_command(char *cmd)
+{
+	char *path, *path_copy *token, *full_path;
+	size_t len;
+
+	if (access(cmd, X_OK) == 0)
+		return (strdup(cmd));
+
+	path = getenv("PATH");
+	if (!path)
+		return (NULL);
+
+	path_copy = strdup(path);
+	if (!path_copy)
+		return (NULL);
+
+		token = strtok(path_copy, ":");
+		while (token)
+		{
+			len = strlen(token) + strlen(cmd) + 2;
+			full_path = malloc(len);
+			if (!full_path)
+				{
+					free(path_copy);
+					return (NULL);
+				}
+			snprintf(full_path, len, "%s/%s", token, cmd);
+			if (access(full_path, X_OK) == 0)
+			{
+				free(path_copy);
+				return (full_path)
+			}
+			free(full_path);
+			token = strtok(NULL, ":");
+		}
+}
+
+/**
+ * main - Simple UNIX shell 0.3 with arguments & PATH handling
  *
  * Return: 0 on success
  */
 
 int main(void)
 {
-	char *line = NULL;
+  char *line = NULL, *command_path;
 	size_t len = 0;
 	ssize_t nread;
 	pid_t pid;
@@ -80,11 +126,19 @@ int main(void)
 			continue;
 		}
 
+		command_path = find_command(args[0]);
+		if (!command_path)
+		  {
+		    fprintf(stderr, "./hsh: Error: %s not located\n", args[0]);
+		    free(args);
+		    continue;
+		  }
+
 		pid = fork();
 		if (pid == 0)
 		{
 
-			if (execve(args[0], args, environ) == -1)
+			if (execve(command_path, args, environ) == -1)
 			{
 				perror("./hsh");
 				exit(EXIT_FAILURE);
@@ -97,12 +151,14 @@ int main(void)
 		else
 		{
 			perror("fork");
+			free(command_path);
 			free(args);
 			free(line);
 			exit(EXIT_FAILURE);
 		}
-
+		free(command_path);
 		free(args);
+		free(line);
 	}
 
 	return (0);
